31904eaf9bfd622132a850998d1dd3daf69b8118
shoaib0657 <99086602+shoaib0657@users.noreply.github.com>
2022-08-28 03:12:51 +0530

    Add files via upload

---
+++ 14.Intersection.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+void intersection(int a[], int b[], int m, int n)
+{
+    int i = 0, j = 0;
+    while(i < m-1 && j < n)
+    {        
+        if((a[i] == a[i-1]) && i > 0)
+        {
+            i++;
+            continue;
+        }
+        if(a[i] > b[j])
+        {
+            j++;
+        }
+        else if(a[i] < b[j])
+        {
+            i++;
+        }
+        else if(a[i] == b[j])
+        {
+            cout << a[i] << " ";
+            i++;
+            j++;
+        }        
+    }
+}
+
+int main()
+{
+    int a[] = {3,5,10,10,10,15,15,20};
+    int b[] = {5,10,10,15,30};
+
+    intersection(a, b, 8, 5);
+
+
+    return 0;
+}
---
+++ 15.Union.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+
+void arrayUnion(int a[], int b[], int m, int n)
+{
+    int i = 0, j = 0;
+    while(i < m && j < n)
+    {
+        if(i >=1 && a[i] == a[i-1])
+        {
+            i++;
+            continue;
+        }
+        if(j >=1 && b[j] == b[j-1])
+        {
+            j++;
+            continue;
+        }
+        if(a[i] < b[j])
+        {
+            cout << a[i] << " ";
+            i++;
+        }
+        else if(a[i] > b[j])
+        {
+            cout << b[j] << " ";
+            j++;
+        }
+        else
+        {
+            cout << a[i] << " ";
+            i++;
+            j++;
+        }
+    }
+
+    while(i < m)
+    {
+        if(i >=1 && a[i] == a[i-1])
+        {
+            i++;
+            continue;
+        }
+        else
+        {
+            cout << a[i] << " ";
+            i++;
+        }
+    }
+    while(j < n)
+    {
+        if(i >=1 && b[j] == b[j-1])
+        {
+            j++;
+            continue;
+        }
+        else
+        {
+            cout << b[j] << " ";
+            j++;
+        }
+    }
+}
+
+int main()
+{
+    int a[] = {3,5,8};
+    int b[] = {2,8,9,10,15};
+
+    arrayUnion(a, b, 3, 5);
+
+
+    return 0;
+}
